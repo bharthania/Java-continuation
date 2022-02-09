6586a3b423dab66b4528c5020c399d39e9108c2c
Firouz <bharthania@gmail.com>
2022-02-09 14:40:32 -0800

    renaming files

--- src/main/java/me/firouz/Continuation.java
+++ src/main/java/me/firouz/Continuation.java
@@ -2,34 +2,51 @@ package me.firouz;
 
 import java.util.Iterator;
 import java.util.Spliterator;
-import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
 import java.util.function.Consumer;
 
 public class Continuation<T> implements Iterable<T>{
-    private Thread thread;
-    private CountDownLatch mainRountineLatch;
-    private CountDownLatch subRountineLatch;
+    private final Thread thread;
+    private final ReentrantLock lock;
+    private final Condition mainRountinCondition;
+    private final Condition subRountinCondition;
     private T value;
 
     public Continuation(Runnable runnable) {
         this.thread = new Thread(runnable);
-        mainRountineLatch = new CountDownLatch(1);
+        lock = new ReentrantLock();
+        mainRountinCondition = lock.newCondition();
+        subRountinCondition = lock.newCondition();
     }
 
     public void yield(T value) throws InterruptedException {
-        this.value = value;
-        subRountineLatch = new CountDownLatch(1);
-        mainRountineLatch.countDown();
-        subRountineLatch.await();
+        try {
+            this.value = value;
+            lock.lock();
+//        subRountineLatch = new CountDownLatch(1);
+//        mainRountineLatch.countDown();
+            mainRountinCondition.signal();
+//        subRountineLatch.await();
+            subRountinCondition.await();
+        }finally {
+            lock.unlock();
+        }
     }
 
     public T goOn() throws InterruptedException {
-        mainRountineLatch = new CountDownLatch(1);
-        if(thread.getState() == Thread.State.NEW) {
-            thread.start();
-        }else subRountineLatch.countDown();
-        mainRountineLatch.await();
-        return value;
+        try {
+            lock.lock();
+//        mainRountineLatch = new CountDownLatch(1);
+            if (thread.getState() == Thread.State.NEW) {
+                thread.start();
+            } else subRountinCondition.signal(); //subRountineLatch.countDown();
+//        mainRountineLatch.await();
+            mainRountinCondition.await();
+            return value;
+        }finally {
+            lock.unlock();
+        }
     }
 
 
